f001fb0f61bf95b65d74e907127286b60faa7eea
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-99@users.noreply.github.com>
2023-06-22 21:39:58 +0800

    Add files via upload

---
+++ NASA3_LOOPING_PERSENTASE.py
@@ -0,0 +1,29 @@
+#KREDIT AHMAD SYAIFUDDIN
+#Looping ini menggunakan Persentase ketika Berjalan menuju suatu nilai yg di inputkan User
+
+
+import random
+import os
+import time
+os.system('cls')
+
+# Daftar kode yang bernilai True
+kodes_benar = [10, 25, 50, 100, 110, 1000, 1114, 56999, 75000]
+
+# Meminta pengguna memasukkan kode
+kode_pengguna = int(input("Masukkan kode : "))
+
+if kode_pengguna in kodes_benar:
+    for i in range(1, kode_pengguna + 1):
+        os.system('cls')
+        progress = (i / kode_pengguna) * 100
+        print("Process {:.2f}%".format(progress))
+        time.sleep(1.25) 
+    print("NASA HACKED")
+else:
+    for i in range(1, kode_pengguna + 1):
+        os.system('cls')
+        progress = (i / kode_pengguna) * 100
+        print("Process {:.2f}%".format(progress))
+        time.sleep(1.25) 
+    print("HACKING FAILED")
